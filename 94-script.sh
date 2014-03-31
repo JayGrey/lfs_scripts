aa781e00da8a69b9f22d7b894911f8f10eb119a3
Jay Grey <k_lite@mail.ru>
2014-03-31 21:08:29 +0300

    Add script on page 94

---
+++ 94-script.sh
@@ -0,0 +1,4 @@
+gcc -dumpspecs | sed -e 's@/tools@@g'				\
+-e '/\*startfile_prefix_spec:/{n;s@.*@/usr/lib/ @}' \
+-e '/\*cpp:/{n;s@$@ -isystem /usr/include@}' > 		\
+`dirname $(gcc --print-libgcc-file-name)`/specs
