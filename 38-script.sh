a5c872cbad960b67a811d7cf1c4dd755fbe9e875
Jay Grey <k_lite@mail.ru>
2014-04-02 08:20:34 +0300

    add script on page 38

---
+++ 38-script.sh
@@ -0,0 +1,10 @@
+../glibc-2.19/configure \
+--prefix=/tools \
+--host=$LFS_TGT \
+--build=$(../glibc-2.19/scripts/config.guess) \
+--disable-profile \
+--enable-kernel=2.6.32 \
+--with-headers=/tools/include \
+libc_cv_forced_unwind=yes \
+libc_cv_ctors_header=yes \
+libc_cv_c_cleanup=yes
